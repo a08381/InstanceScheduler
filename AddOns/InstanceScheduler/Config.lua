8d10f6544d8a5508b22bdfc1d517425ab8839b0f
a08381 <632785425@163.com>
2017-03-30 09:08:17 +0800

    init.

---
+++ AddOns/InstanceScheduler/Config.lua
@@ -0,0 +1,24 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: lenovo
+-- Date: 2017/3/28
+-- Time: 23:48
+-- To change this template use File | Settings | File Templates.
+--
+
+InstanceScheduler.PrintPrefix = "|cff99ffffInstance Schedule|r - "
+InstanceScheduler.AutoStart = true
+InstanceScheduler.IsPaid = true
+InstanceScheduler.TempTime = GetTime()
+InstanceScheduler.PaidRealm = { "织亡者", "屠魔山谷", "外域", "阿格拉玛" }
+InstanceScheduler.Keywords = { "CD", "无敌", "飞机头", "星光龙", "季鹍" }
+InstanceScheduler.Messages =
+{
+    AddBlacklistSuccess = "添加玩家到黑名单成功~",
+    AlreadyInBlacklist = "这名玩家已经在黑名单里了哦~",
+    OthersInGroup = "当前队伍里有其他人在哦，请稍后再尝试M我1哦~~~",
+    GroupWelcome = "副本已重置，请进本，进本后请在小队频道里发1~~~",
+    ChangeLeader = "已将队长转交，刷无敌请自行修改难度为英雄，修改难度完毕或确认后请打1~~~",
+    Finish = "服务完成~~祝您刷出想要的坐骑~~~",
+    Notice = "诚信服务，500G/本~~~一定要记得邮哦~~~",
+}
