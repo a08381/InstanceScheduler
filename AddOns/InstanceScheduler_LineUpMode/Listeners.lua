cd21c3277889d03297865df5398d2b603d10cd9d
a08381 <632785425@163.com>
2017-05-21 09:58:13 +0800

    重建架构.

---
+++ AddOns/InstanceScheduler_LineUpMode/Listeners.lua
@@ -0,0 +1,106 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: lenovo
+-- Date: 2017/5/20
+-- Time: 18:07
+-- To change this template use File | Settings | File Templates.
+--
+
+InstanceScheduler["CHAT_MSG_WHISPER"] = function (...)
+    local _, message, sender = ...
+    if message:sub(1, 1) == "1" and sender ~= InstanceScheduler:NameFormat(UnitName("player")) then
+        for i,v in ipairs(InstanceSchedulerVariables.Line) do
+            if sender == v then
+                InstanceScheduler:SendWhisperMessage("AlreadyInLine", sender, i)
+                if not IsInGroup() and #InstanceSchedulerVariables.Line > 0 then
+                    local name = InstanceSchedulerVariables.Line[1]
+                    table.remove(InstanceSchedulerVariables.Line, 1)
+                    InviteUnit(name)
+                end
+                return
+            end
+        end
+        if IsInGroup() then
+            if not UnitInParty(sender) then
+                table.insert(InstanceSchedulerVariables.Line, sender)
+                InstanceScheduler:SendWhisperMessage("AddInLine", sender)
+            end
+        else
+            table.insert(InstanceSchedulerVariables.Line, sender)
+            if InstanceSchedulerVariables.Line[1] == sender and not IsInGroup() then
+                InviteUnit(sender)
+                table.remove(InstanceSchedulerVariables.Line, 1)
+            else
+                InstanceScheduler:SendWhisperMessage("AddInLine", sender)
+            end
+        end
+    end
+end
+
+InstanceScheduler["CHAT_MSG_PARTY"] = function (...)
+    local _, message, sender = ...
+    if message:len() >= 2 and message:sub(1, 2) == "10" and sender ~= InstanceScheduler:NameFormat(UnitName("player")) then
+        SetLegacyRaidDifficultyID(3)
+        InstanceScheduler:SendPartyMessage("ChangeDifficulty")
+    end
+end
+
+InstanceScheduler["CHAT_MSG_GUILD"] = function (...)
+    local CommandPrefix, _, message = InstanceScheduler.Commands.CommandPrefix, ...
+    if message:len() >= CommandPrefix:len() and message:sub(1, CommandPrefix:len()) == CommandPrefix then
+        local arg = message:sub(CommandPrefix:len() + 1, -1)
+        if arg:len() >= 4 and arg:sub(1, 4) == "info" then
+            InstanceScheduler:CommandInfo(arg:len()==4 and nil or arg:sub(5, -1))
+        end
+    end
+end
+
+InstanceScheduler["PARTY_INVITE_REQUEST"] = function (...)
+    DeclineGroup()
+end
+
+InstanceScheduler["GROUP_ROSTER_UPDATE"] = function (...)
+    if IsInGroup() then
+        if GetNumGroupMembers() == 1 then
+            InstanceScheduler:InviteSchedule(0)
+        end
+        if GetNumGroupMembers() == 2 and UnitIsGroupLeader("player") and InstanceScheduler.InGroupPlayer ~= InstanceScheduler:NameFormat(UnitName("party1")) then
+            InstanceScheduler.InGroupPlayer = InstanceScheduler:NameFormat(UnitName("party1"))
+            InstanceScheduler:PartySchedule(0)
+        end
+    else
+        if InstanceScheduler.InGroupPlayer ~= "" then
+            InstanceScheduler.InGroupPlayer = ""
+        end
+        if GetLegacyRaidDifficultyID() == 3 then
+            SetLegacyRaidDifficultyID(4)
+        end
+        if InstanceScheduler.TempMembers == 1 or InstanceScheduler.TempMembers == 2 then
+            if #InstanceSchedulerVariables.Line > 0 then
+                local name = InstanceSchedulerVariables.Line[1]
+                table.remove(InstanceSchedulerVariables.Line, 1)
+                InviteUnit(name)
+            end
+        end
+    end
+    InstanceScheduler.TempMembers = IsInGroup() and GetNumGroupMembers() or 0
+end
+
+InstanceScheduler["VARIABLES_LOADED"] = function (...)
+    local frame = ...
+    if not InstanceSchedulerVariables then
+        InstanceSchedulerVariables =
+        {
+            Line = {},
+            Users = {},
+        }
+    end
+    frame:UnregisterEvent("VARIABLES_LOADED")
+    if InstanceScheduler.AutoStart then
+        frame:RegisterEvent("CHAT_MSG_WHISPER")
+        frame:RegisterEvent("CHAT_MSG_PARTY")
+        frame:RegisterEvent("PARTY_INVITE_REQUEST")
+        frame:RegisterEvent("GROUP_ROSTER_UPDATE")
+        frame:SetScript("OnUpdate", InstanceScheduler.UpdateSchedule)
+    end
+end
---
+++ AddOns/InstanceScheduler_LineUpMode/Schedulers.lua
@@ -0,0 +1,89 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: lenovo
+-- Date: 2017/5/20
+-- Time: 18:08
+-- To change this template use File | Settings | File Templates.
+--
+
+function InstanceScheduler:InviteSchedule(times)
+    if IsInGroup() and GetNumGroupMembers() == 1 then
+        if times >= 5 then
+            LeaveParty()
+        else
+            C_Timer.After(1, function()
+                self:InviteSchedule(times+1)
+            end)
+        end
+    end
+end
+
+function InstanceScheduler:PartySchedule(times)
+    if IsInGroup() then
+        if UnitIsConnected("party1") then
+            ResetInstances()
+            self:SendPartyMessage("ResetComplete")
+            self.InGroupTime = GetTime()
+            C_Timer.After(1, function()
+                self:IntoInstanceSchedule()
+            end)
+        else
+            if times >= 6 then
+                local name,realm = UnitName("party1")
+                local s = self:NameFormat(name, realm, true)
+                LeaveParty()
+                self:SendWhisperMessage("NetProblem", s)
+            else
+                C_Timer.After(1, function()
+                    self:PartySchedule(times+1)
+                end)
+            end
+        end
+    end
+end
+
+function InstanceScheduler:IntoInstanceSchedule()
+    if IsInGroup() then
+        if not GetPlayerMapPosition("party1") then
+            local name,realm = UnitName("party1")
+            local s = self:NameFormat(name, realm, true)
+            PromoteToLeader(s)
+            self:SendPartyMessage("ChangeLeader")
+            C_Timer.After(1, function()
+                if IsInGroup() then
+                    self:SendPartyMessage("LeaveMessage")
+                    C_Timer.After(1, function()
+                        LeaveParty()
+                        local fullname = self:NameFormat(name, realm)
+                        local times = InstanceSchedulerVariables.Users[fullname]
+                        if times then
+                            InstanceSchedulerVariables.Users[fullname] = times + 1
+                        else
+                            InstanceSchedulerVariables.Users[fullname] = 1
+                        end
+                    end)
+                end
+            end)
+        else
+            if not UnitIsConnected("party1") then
+                LeaveParty()
+            elseif GetTime() - self.InGroupTime > 60 and #InstanceSchedulerVariables.Line > 0 then
+                LeaveParty()
+            else
+                C_Timer.After(1, function()
+                    self:IntoInstanceSchedule()
+                end)
+            end
+        end
+    end
+end
+
+function InstanceScheduler:UpdateSchedule()
+    local t = GetTime()
+    if StaticPopup1:IsShown() and StaticPopup1Button1:GetText() == "取消" then
+        if t - InstanceScheduler.TempTime > 1 then
+            StaticPopup1Button1:Click()
+            InstanceScheduler.TempTime = t
+        end
+    end
+end
